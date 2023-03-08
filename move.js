4fca159d685e8db9c631e5bcd2b40c27e351b1fc
Nikita Kudinov <89845201+karamawari0@users.noreply.github.com>
2023-03-08 20:43:37 +0300

    Update move.js

--- move.js
+++ move.js
@@ -1,13 +1,17 @@
 function move() {
-  var sourceSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Пакеты");
+  var NAME_OF_SOURCE_SHEET = "XXX";
+  var NAME_OF_TARGET_SHEET = "YYY";
+  var WORD_TO_SORT_BY = "NNN";
+  
+  var sourceSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(NAME_OF_SOURCE_SHEET);
   var rows = sourceSheet.getDataRange().getValues();
-  var targetSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Курсы");
+  var targetSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(NAME_OF_TARGET_SHEET);
   var rowsToBeDeleted = [];
   var rowsDeleted = 0;
 console.log(rows)
 
  for(var i=1; i<rows.length; i++){
-    if(rows[i][0].includes("Пакет")==false)
+    if(rows[i][0].includes(WORD_TO_SORT_BY)==false)
     {
      rowsToBeDeleted.push(i+1)
      targetSheet.appendRow(rows[i]);
